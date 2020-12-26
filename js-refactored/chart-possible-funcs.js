3173de0269608511abe66774bfb6af9e970dea8f
Akil Hashmi <ah858@cam.ac.uk>
2020-12-26 11:26:52 +0000

    Restyle possible-funcs line

--- js-refactored/chart-possible-funcs.js
+++ js-refactored/chart-possible-funcs.js
@@ -64,8 +64,11 @@ function drawChartPossibleFuncs() {
 
   // Model parameters
   const modelLineGroup = svg.append("g")
-    .attr("stroke", colors[1])
-    .attr("stroke-width", 4)
+    // .attr("stroke", colors[1])
+    // .attr("stroke-width", 4)
+    .attr("stroke", colors[2])
+    .attr("stroke-dasharray", (3, 5)) // match this to the underlying function style used later
+    .attr("stroke-width", 2)
     .attr('fill', 'none');
 
   svg.append("g")
